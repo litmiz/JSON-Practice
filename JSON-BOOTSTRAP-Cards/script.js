17f7c350753a085a330764a4e5df012ff720906a
litmiz <litmiz07@gmail.com>
2020-06-17 18:48:39 +0300

    Create JS File

---
+++ JSON-BOOTSTRAP-Cards/script.js
@@ -0,0 +1,14 @@
+// Create Arr //
+
+allusers = [];
+allusers = users.users;
+
+/////////////////////////
+
+// Add users to HTML with DOM //
+
+allusers.forEach(element => {
+    
+});
+
+/////////////////////////
